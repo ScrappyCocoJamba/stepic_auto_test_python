f9de04e5e6423f4af9a3ebb7e0cd709d67df5623
ScrappyCoco <123456Qwe#$%>
2020-11-11 16:28:53 +0700

    Add a tasks from module 1

---
+++ module 1/lesson 6 step 5.py
@@ -0,0 +1,28 @@
+import math
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+    browser.get('http://suninjuly.github.io/find_link_text')
+    # расшифровка кода из цифр "224592" для нахождения текста ссылки
+    search_text = str(math.ceil(math.pow(math.pi, math.e)*10000))
+    # поиск ссылки по тексту "224592" и переход по ней
+    link = browser.find_element_by_link_text(search_text)
+    link.click()
+    # заполение формы
+    input1 = browser.find_element_by_tag_name("Input")
+    input1.send_keys("Konstantin")
+    input2 = browser.find_element_by_name("last_name")
+    input2.send_keys("Martyashkov")
+    input3 = browser.find_element_by_class_name("city")
+    input3.send_keys("Novosibirsk")
+    input4 = browser.find_element_by_id("country")
+    input4.send_keys("Russia")
+    # получение кода
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    time.sleep(20)
+
+finally:
+    browser.quit()
