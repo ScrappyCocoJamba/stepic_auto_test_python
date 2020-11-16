39248f6fc450073f250066b6dd2f6b645cc8b856
ScrappyCoco <123456Qwe#$%>
2020-11-16 17:25:16 +0700

    Module 3 add a task

---
+++ module 3/lesson 2 step 9.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+
+'''
+Форматирование строк
+https://realpython.com/python-string-formatting/#2-new-style-string-formatting-strformat
+
+F-strings
+https://realpython.com/python-string-formatting/#3-string-interpolation-f-strings-python-36
+
+
+'''
+# browser = webdriver.Chrome()
+
+s = 'My Name is Julia'
+
+if 'Name' in s:
+    print('Substring found')
+
+index = s.find('Name')
+if index != -1:
+    print(f'Substring found at index {index}')
+
+#  assert "login" in browser.current_url, # сообщение об ошибке
+
+
+def test_substring(full_string, substring):
+    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
+
