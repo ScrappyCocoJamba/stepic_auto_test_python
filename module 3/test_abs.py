43d2bd8f10115a6511d5d6a278e2c0e84bfd437f
ScrappyCocoJamba <konstantinmart10@gmail.com>
2020-12-02 17:53:11 +0700

    unittest vs. pytest

---
+++ module 3/test_abs.py
@@ -0,0 +1,8 @@
+
+# функция проверки абсолютного значения
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+# функция проверки абсолютного значения
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
