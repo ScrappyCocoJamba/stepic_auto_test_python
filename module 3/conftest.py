0704a467169356fe67812081e0bfae852423767e
ScrappyCocoJamba <konstantinmart10@gmail.com>
2020-12-18 15:48:06 +0700

    conftest file with addoption(parser) to choose browser for tests

---
+++ module 3/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+
+#  добавляем парсер при запуске `pytest -s -v --browser_name=chrome test_parser.py` передать название браузера
+#  также можно прописать по дефолту открывать браузер без передачи параметра
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
