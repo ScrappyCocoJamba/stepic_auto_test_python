d2166f8c84d0467bd14786fbb1a6943bf3a2cade
ScrappyCocoJamba <konstantinmart10@gmail.com>
2020-12-24 17:29:07 +0700

    1

--- module 3/test_abs.py
+++ module 3/test_abs.py
@@ -1,7 +1,7 @@
-
 # функция проверки абсолютного значения
 def test_abs1():
     assert abs(-42) == 42, "Should be absolute value of a number"
+
 
 # функция проверки абсолютного значения
 def test_abs2():
